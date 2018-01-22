5cf7fed9579b069cf36aafbe212738fd8997cba5
baonguyen96 <baonguyendrump@gmail.com>
2018-01-22 08:52:07 -0600

    Add Demo Popups

--- clientScript/dynamicContents/projects.js
+++ clientScript/dynamicContents/projects.js
@@ -1,11 +1,13 @@
+// NOTE: demos are recorded at resolutions: 1024×576 (aspect ratio:  16:9)
+
 var projectsList = [
     {
         "title": "Dark Chroma",
         "intro": createIntro("A Chrome extension that revert the color of the page (bright text on dark background) to reduce eye-strains from long working period.", ["Chrome extension"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/DarkChroma"),
-            createPointer() + createLinkText("https://chrome.google.com/webstore/detail/dark-chroma/jjknnbjoocbcgemnnjoaiifopndlcnbe", "Also available on", "Chrome Web Store", true)
+            createLinkTextWithPointer("https://github.com/baonguyen96/DarkChroma"),
+            createLinkTextWithPointer("https://chrome.google.com/webstore/detail/dark-chroma/jjknnbjoocbcgemnnjoaiifopndlcnbe", "Also available on", "Chrome Web Store")
         ]
     },
     {
@@ -13,7 +15,7 @@ var projectsList = [
         "intro": createIntro("This Java project implements a unique protocol for File Transfer between one Server and one Client utilizing Socket Programming, Connection Management, Reliable Communication, and Security (Public Key Crypto as well as Symmetric Key Crypto).", ["Java"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/FileTransferApplication")
+            createLinkTextWithPointer("https://github.com/baonguyen96/FileTransferApplication")
         ]
     },
     {
@@ -21,14 +23,15 @@ var projectsList = [
         "intro": createIntro("This Java, Java Swing project provides conversion from and to Java, MIPS, and Machine codes. It recognizes different instruction types (I-, R-, or J-) from MIPS or Binary codes and provides the other two equivalences on a GUI.", ["Java", "Java Swing"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/MipsConverter")
+            createLinkTextWithPointer("https://github.com/baonguyen96/MipsConverter")
         ]
     },
     {
         "title": "Morse Mastering",
         "intro": createIntro("An Android application that teaches users the Morse code and translates between Morse and normal text. It also provides some Android services, such as Notification and Network Communication.", ["Android application"]),
         "links": [
-            createPointer() + createLinkText("https://github.com/baonguyen96/MorseMastering")
+            createDemoText(),
+            createLinkTextWithPointer("https://github.com/baonguyen96/MorseMastering")
         ]
     },
     {
@@ -36,7 +39,7 @@ var projectsList = [
         "intro": createIntro("This C++ project  simulates the current online ticketing systems - that either reserves the seats with receipts then flies the plane when full, or puts the selected seats onto the wait-list if those seats are not available. The prices are based on the type of seat(s) and the age of user.", ["C++"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/AirplaneTicketsReservingSystem")
+            createLinkTextWithPointer("https://github.com/baonguyen96/AirplaneTicketsReservingSystem")
         ]
     },
     {
@@ -49,7 +52,7 @@ var projectsList = [
         "intro": createIntro("This C# project represents a map of flights that connect different cities. Each city is a vertex, and each flight is an edge in the graph. It utilizes Dijkstra's algorithm to find the 3 shortest paths (either by flight duration or by total distance) between given 2 cities.", ["C#"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/FlightGraph")
+            createLinkTextWithPointer("https://github.com/baonguyen96/FlightGraph")
         ]
     },
     {
@@ -57,7 +60,7 @@ var projectsList = [
         "intro": createIntro("Simple Java, JavaFX program to calculate the difference between 2 dates, or the second date given the first one and the duration.", ["Java", "JavaFX"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/Calendar")
+            createLinkTextWithPointer("https://github.com/baonguyen96/Calendar")
         ]
     },
     {
@@ -65,7 +68,7 @@ var projectsList = [
         "intro": createIntro("A docking software system that tracks parking lot's activities and generates violation tickets, written in C++.", ["C++"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/TicketToKnowhere")
+            createLinkTextWithPointer("https://github.com/baonguyen96/TicketToKnowhere")
         ]
     },
     {
@@ -73,7 +76,7 @@ var projectsList = [
         "intro": createIntro(createLinkText("https://en.wikipedia.org/wiki/Red%E2%80%93black_tree", "This is my own implementation of the", "Red Black Tree") + " data structure in Java.", ["Java"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/RedBlackTree")
+            createLinkTextWithPointer("https://github.com/baonguyen96/RedBlackTree")
         ]
     },
     {
@@ -81,7 +84,7 @@ var projectsList = [
         "intro": createIntro("This project is a statistical analysis program based on the concept of multi-sensors data fusion - the process of combining observations from a number of different sensors to provide a robust and complete description of an environment or process. Written in C.", ["C"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/HealthScore")
+            createLinkTextWithPointer("https://github.com/baonguyen96/HealthScore")
         ]
     },
     {
@@ -89,23 +92,21 @@ var projectsList = [
         "intro": createIntro(createLinkText("#FileTransferApplication", "A simple chat room project in Java that allows a client and server to talk to each other. This is a \"smaller, simpler\" version of the", " File Transfer Application") + " project.", ["Java"]),
         "links": [
             createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/Chatroom")
+            createLinkTextWithPointer("https://github.com/baonguyen96/Chatroom")
         ]
     },
     {
         "title": "KWIC System",
         "intro": createIntro(createLinkText("https://en.wikipedia.org/wiki/Key_Word_in_Context", "This group project is a web based solution to the", "Key Word in Context", true) + " problem, implemented using HTML, CSS, JavaScript, and JSP.", ["HTML", "CSS", "JavaScript", "JSP"]),
         "links": [
-            createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/KWIC_Project")
+            createLinkTextWithPointer("https://github.com/baonguyen96/KWIC_Project")
         ]
     },
     {
         "title": "Cyberminer",
         "intro": createIntro(createLinkText("#KWICSystem", "A simple search engine built in HTML, CSS, JavaScript, JSP, MySQL, and Selenium that allows users to add a URL-Description tuple to the database, and carries out a search on the descriptions. Built upon the", "KWIC") + " project.", ["HTML", "CSS", "JavaScript", "JSP", "MySQL", "Selenium"]),
         "links": [
-            createDemoText(),
-            createPointer() + createLinkText("https://github.com/baonguyen96/Cyberminer")
+            createLinkTextWithPointer("https://github.com/baonguyen96/Cyberminer")
         ]
     }
 ];
@@ -117,17 +118,22 @@ function createPointer() {
 
 
 function createDemoText() {
-    return createPointer() + "See <span class='linkText'>Demo</span>";
+    return createPointer() + "See <a href='#' class='demoLink' data-toggle='modal' data-target='#hiddenModal'>Demo</a>";
 }
 
 
-function createLinkText(link, welcome, name) {
+function createLinkTextWithPointer(link, welcome, name) {
     // default is GitHub
     if(arguments.length === 1) {
         welcome = "Check out on";
         name = "GitHub";
     }
 
+    return createPointer() + createLinkText(link, welcome, name);
+}
+
+
+function createLinkText(link, welcome, name) {
     return welcome + " <a href=\"" + link + "\" target=\"_blank\">" + name + "</a>";
 }
 
@@ -182,27 +188,22 @@ function addProjectsToGroup(groupElement, fromProjectIndex, toProjectIndex) {
 
 
 function showDemo() {
-    $(".projectLink .linkText").click(function () {
-        var modal = $("#modalTemplate").find(".modal").clone();
-        var closeModalButton = modal.find(".close");
-        var projectTitle = $(this).closest(".project").find(".projectTitle").text();
+    var myModal = $("#hiddenModal");
+    var demoProjectId = "";
+    var demoProjectTitle = "";
 
-        // modal.find(".modalHeader").find("h2").html(projectTitle);
-        modal.css("display", "block");
-
-        console.log(modal.html());
-        console.log(projectTitle);
-
-        // closeModalButton.click(function () {
-        //     modal.hide();
-        // });
-        //
-        // $(window).click(function (event) {
-        //     if (event.target === modal) {
-        //         modal.show();
-        //     }
-        // })
+    $(".projectLink .demoLink").click(function () {
+        demoProjectTitle = $(this).closest(".project").find(".projectTitle").text();
+        demoProjectId = $(this).closest(".project").attr("id");
     });
+
+    // Show loader & then get content when modal is shown
+    myModal.on('show.bs.modal', function(e) {
+        $(this).find(".modal-title").text(demoProjectTitle);
+        $(this).find(".demoImage").attr("src", "../assets/images/demos/" + demoProjectId + ".gif");
+        $(this).find(".demoImage").attr("alt", demoProjectTitle + " Demo");
+    });
+
 }
 
 
@@ -216,4 +217,6 @@ $(function () {
 
     // from project 5, only show when user requests
     addProjectsToGroup(projectsHidden, 4, projectsList.length);
+
+    showDemo();
 });
